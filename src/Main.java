0b53bd554d6dfb5f6fd58106c17f6bbcc668d08b
Repook <supercoopergaming123@gmail.com>
2024-04-11 21:09:41 +1200

    version 1

--- src/Main.java
+++ src/Main.java
@@ -12,96 +12,23 @@ public class Main {
         boolean keepPlaying = true;
         Scanner scanner = new Scanner(System.in);
 
+        String[] mathQuestions = {"Whats 50 + 12?","Whats 2 x 7?","Whats 43 - 4?","Whats 20 / 2?"};
+        String[] mathAnswers = {"62","14","39","10"};
 
-        //math question and answers
-        ArrayList<String> mathQuestions = new ArrayList<>();
-        mathQuestions.add("Whats 50 + 12?");
-        mathQuestions.add("Whats 2 x 7?");
-        mathQuestions.add("Whats 43 - 4?");
-        mathQuestions.add("Whats 20 / 2?");
-
-        ArrayList<String> mathAnswers = new ArrayList<>();
-        mathAnswers.add("62");
-        mathAnswers.add("14");
-        mathAnswers.add("39");
-        mathAnswers.add("10");
-
-        //geography question and answers
-        ArrayList<String> geoQuestions = new ArrayList<>();
-        geoQuestions.add("What is the capital of France?");
-        geoQuestions.add("What city has the largest population in New Zealand?");
-        geoQuestions.add("What does UK stand for?");
-        geoQuestions.add("What country has the biggest population?");
-
-        ArrayList<String> geoAnswers = new ArrayList<>();
-        geoAnswers.add("PARIS");
-        geoAnswers.add("AUCKLAND");
-        geoAnswers.add("UNITED KINGDOM");
-        geoAnswers.add("CHINA");
-
-        //english questions and answers
-        ArrayList<String> engQuestions = new ArrayList<>();
-        engQuestions.add("Who wrote Hamlet?");
-        engQuestions.add("What word is used to identify people, places, or things?");
-        engQuestions.add("How many letters are in the alphabet?");
-        engQuestions.add("What figure of speech involves comparing things using the words 'like' or 'as'?");
-
-        ArrayList<String> engAnswers = new ArrayList<>();
-        engAnswers.add("SHAKESPEARE");
-        engAnswers.add("NOUN");
-        engAnswers.add("26");
-        engAnswers.add("SIMILE");
-
-
-        //Introduction
-        System.out.println("Welcome to the TEST. We will need you to answer the questions provided.\nBut first, what is your name?");
-        //gets the user's name and saves it for later
-        String username = scanner.nextLine();
-        System.out.println("Welcome " + username + " to the TEST. Please answer the questions provided.");
 
         //while loop so that if the user wants they can redo the quiz
         while (keepPlaying) {
             rightAnswers = 0;
             boolean yesOrNo = true;
-            boolean checkSubjectQuestions = true;
-            //asks what set of questions the user would like to do
-            System.out.println("What set of question would you like to do?\nMath[1]\nEnglish[2]\nGeography[3]?");
 
             //saves the input of the user in a string, this is done as if it was saved as an int it caused issues
-            String whichQuestions = scanner.nextLine();
+            //String whichQuestions = scanner.nextLine();
 
             //checks what section of questions the user would like to do
-            while (checkSubjectQuestions) {
-                switch (whichQuestions) {
-                    case "1" -> {
-                        //math questions
-                        int answers = mathAnswers.size();
-                        answerAllTheQuestionsRandom(mathQuestions, mathAnswers, scanner);
-                        System.out.println("You got " + rightAnswers + "/" + answers + " questions right " + username + "!");
-                        checkSubjectQuestions = false;
-                    }
-                    case "2" -> {
-                        //english questions
-                        int answers = engAnswers.size();
-                        answerAllTheQuestionsRandom(engQuestions, engAnswers, scanner);
-                        System.out.println("You got " + rightAnswers + "/" + answers + " questions right " + username + "!");
-                        checkSubjectQuestions = false;
-                    }
-                    case "3" -> {
-                        //geography questions
-                        int answers = geoAnswers.size();
-                        answerAllTheQuestionsRandom(geoQuestions, geoAnswers, scanner);
-                        System.out.println("You got " + rightAnswers + "/" + answers + " questions right" + username + "!");
-                        checkSubjectQuestions = false;
-                    }
-                    case null, default -> {
-                        //this occurs if the user specifically did not enter 1,2, or 3
-                        System.out.println("That is not a valid answer. Please type 1, 2, or 3.");
-                        System.out.println("What set of question would you like to do?\nMath[1]\nEnglish[2]\nGeography[3]?");
-                        whichQuestions = scanner.nextLine();
-                    }
-                }
-            }
+
+            answerAllTheQuestions(mathQuestions, mathAnswers, scanner);
+            System.out.println("You got " + rightAnswers + "/" + mathAnswers.length + " questions right " + "!");
+
             //Asks the user after they finish a section if they would like to quit
             while (yesOrNo) {
                 questionlength = 1;
@@ -132,7 +59,7 @@ public class Main {
     //is deleted so you dont get repeated questions
 
 
-    private static void answerAllTheQuestionsRandom(ArrayList<String> questions, ArrayList<String> answers, Scanner scanner) {
+/*    private static void answerAllTheQuestionsRandom(ArrayList<String> questions, ArrayList<String> answers, Scanner scanner) {
         //loops for the amount of questions there is
         ArrayList clonedquestions = (ArrayList) questions.clone();
         ArrayList clonedanswers = (ArrayList) answers.clone();
@@ -156,10 +83,9 @@ public class Main {
             clonedanswers.remove(randint);
             questionlength++;
         }
-    }
-}
+    }*/
 
-    /*    private static void answerAllTheQuestions(String[] questions, String[] answers, Scanner scanner){
+    private static void answerAllTheQuestions(String[] questions, String[] answers, Scanner scanner){
         //loops for the amount of questions there is
         for (int i = 0; i < questions.length; i++) {
             System.out.println(questions[i]);
@@ -173,4 +99,6 @@ public class Main {
                 System.out.println("Wrong!");
             }
         }
-    }*/
+    }
+}
+
